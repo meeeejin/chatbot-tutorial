1e81f98e169ccd30ed4fb9e5788e969db6e88261
meeeejin <meeeeejin@gmail.com>
2019-04-29 02:27:17 -0700

    Used quick reponses

--- app.js
+++ app.js
@@ -86,7 +86,17 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`,
+      quick_replies: [{
+        content_type: "text",
+        title: "dogs",
+        //payload: "quick_menu"
+      }, {
+          content_type: "text",
+          title: "cats"  ,
+          image_url: "http://example.com/img/rabbit.png",
+          //payload: "quick_next_quiz"
+      }]
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
