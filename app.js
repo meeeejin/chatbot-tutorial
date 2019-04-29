dc0352ad74976afc5a05b941d806103b555018bc
meeeejin <meeeeejin@gmail.com>
2019-04-29 03:29:17 -0700

    Handle the payload

--- app.js
+++ app.js
@@ -32,11 +32,12 @@ app.post('/webhook', (req, res) => {
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
-      if (webhook_event.message) {
+      if (webhook_event.message.quick_reply.payload) {
+        handlePostback(sender_psid, webhook_event.postback);
+      } else if (webhook_event.message) {
         console.log(webhook_event.message);
         handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
-        console.log(webhook_event.postback)
         handlePostback(sender_psid, webhook_event.postback);
       }
       
